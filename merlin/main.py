c32d809f7196a875cecb699682e2ed7a6cd8d1e6
AaronTheNerd <bargeaaron@gmail.com>
2023-10-25 21:52:03 -0400

    Chore: Renamed src to merlin

---
+++ merlin/main.py
@@ -0,0 +1,14 @@
+#!/home/aaronthenerd/.local/share/virtualenvs/Merlin-NowmQrlJ/bin/python3.10
+
+def main():
+    # Get name of project
+    # Determine python version
+    # Determine if user wants
+    # - config.json file
+    # - argument parser
+    # - logging
+    # - unit testing
+    # Create everything
+
+if __name__ == "__main__":
+    main()
