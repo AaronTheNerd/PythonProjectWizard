1bba0f65f91fbb5292bad89bc36cd6510f5bbdec
AaronTheNerd <bargeaaron@gmail.com>
2023-11-01 18:44:23 -0400

    Feature: Added yes_or_no_validator function and a ValidatorException class

--- merlin/test/test_validtor.py
+++ merlin/test/test_validtor.py
@@ -1,8 +1,33 @@
 import unittest
 from typing import Callable
 
-from merlin.validator import name_validator
+from merlin.validator import name_validator, yes_or_no_validator, ValidatorException
 
 class ValidatorTestSuite(unittest.TestCase):
     def test_name_validator_callable(self):
         self.assertIsInstance(name_validator, Callable)
+
+    def test_name_validator(self):
+        raw_input = "merlin"
+        answer = name_validator(raw_input)
+        self.assertEquals(answer.value, raw_input)
+
+    def test_yes_or_no_validator_callable(self):
+        self.assertIsInstance(yes_or_no_validator, Callable)
+
+    def test_yes_or_no_validator_true_values(self):
+        raw_inputs = ["Y", "y", "Yes", "Yeah", "yes", "yup", "you"]
+        for raw_input in raw_inputs:
+            answer = yes_or_no_validator(raw_input)
+            self.assertTrue(answer.value)
+
+    def test_yes_or_no_validator_false_values(self):
+        raw_inputs = ["N", "n", "No", "no", "Nah", "nay"]
+        for raw_input in raw_inputs:
+            answer = yes_or_no_validator(raw_input)
+            self.assertFalse(answer.value)
+
+    def test_yes_or_no_validator_error_values(self):
+        raw_inputs = ["maybe", "c", "C", "huh"]
+        for raw_input in raw_inputs:
+            self.assertRaises(ValidatorException, yes_or_no_validator, raw_input)
--- merlin/validator.py
+++ merlin/validator.py
@@ -1,7 +1,19 @@
+import re
 from typing import Callable
+
 from merlin.answer import Answer
 
 Validator = Callable[[str], Answer]
 
+class ValidatorException(Exception):
+    ...
+
 def name_validator(raw_input: str) -> Answer:
-    return Answer()
+    return Answer(raw_input)
+
+def yes_or_no_validator(raw_input: str) -> Answer: 
+    true_input = re.match("^y", raw_input, re.IGNORECASE)
+    if true_input: return Answer(True)
+    false_input = re.match("^n", raw_input, re.IGNORECASE)
+    if false_input: return Answer(False)
+    raise ValidatorException(f"Unexpected valued received: `{raw_input}`.\nPlease input yes or no.")
