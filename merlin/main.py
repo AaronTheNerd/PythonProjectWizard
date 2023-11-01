3c54927a078e335d59c9f68477bed4386c51db65
AaronTheNerd <bargeaaron@gmail.com>
2023-10-31 23:51:01 -0400

    Feature: Began to fill in the structure of the main function

--- merlin/main.py
+++ merlin/main.py
@@ -1,14 +1,20 @@
 #!/home/aaronthenerd/.local/share/virtualenvs/Merlin-NowmQrlJ/bin/python3.10
 
+from merlin.dialog.project_dialog import ProjectDialog
+from merlin.display.console import Console
+from merlin.question_suite import QuestionSuite
+from merlin.question import Question
+
 def main():
-    # Get name of project
-    # Determine python version
-    # Determine if user wants
-    # - config.json file
-    # - argument parser
-    # - logging
-    # - unit testing
+    # Run dialog
+    console = Console()
+    question_suite = QuestionSuite({
+        "name": Question("What is the name of your Project?")
+    })
+    dialog = ProjectDialog(console, question_suite)
+    project = dialog.run()
     # Create everything
+    ...
 
 if __name__ == "__main__":
     main()
