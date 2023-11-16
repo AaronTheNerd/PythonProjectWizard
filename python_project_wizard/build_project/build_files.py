5399580bb768a3cf1742d59ea7dac6bda24c1ac0
AaronTheNerd <bargeaaron@gmail.com>
2023-11-15 21:39:32 -0500

    Feature: Added code to generate and build launch.json files based on project answers

--- python_project_wizard/build_project/build_files.py
+++ python_project_wizard/build_project/build_files.py
@@ -1,13 +1,31 @@
 import os
 
 from python_project_wizard.build_project.directories import Directories
+from python_project_wizard.build_project.get_launch_json_content import (
+    get_launch_json_content,
+)
 from python_project_wizard.project import Project
 
 
-def build_files(project: Project, directories: Directories):
-    build_file(os.path.join(directories.main, "README.md"), f"# {project.name}")
+def build_files(project: Project, directories: Directories) -> None:
+    build_static_files(project, directories)
+    build_launch_json(project, directories)
+    build_gist_files(project, directories)
 
 
-def build_file(path: str, content: str):
+def build_static_files(project: Project, directories: Directories) -> None:
+    build_file(os.path.join(directories.main, "README.md"), f"# {project.name.title()}")
+
+
+def build_launch_json(project: Project, directories: Directories) -> None:
+    content = get_launch_json_content(project)
+    build_file(os.path.join(directories.dot_vscode, "launch.json"), content)
+
+
+def build_gist_files(project: Project, directories: Directories) -> None:
+    ...
+
+
+def build_file(path: str, content: str) -> None:
     with open(path, "w+") as file:
         file.write(content)
---
+++ python_project_wizard/test/test_build_files.py
@@ -0,0 +1,30 @@
+import os
+import unittest
+import unittest.mock as mock
+
+from python_project_wizard.build_project.build_files import *
+from python_project_wizard.project import Project
+from python_project_wizard.build_project.get_launch_json_content import *
+
+
+class BuildFilesTestSuite(unittest.TestCase):
+    @mock.patch("builtins.open")
+    def test_build_file(self, mocked_open: mock.Mock):
+        filepath = os.path.join(os.getcwd(), "file_test1.txt")
+        build_file(filepath, "Hello, World")
+        call = [mock.call(filepath, "w+")]
+        mocked_open.assert_has_calls(call)
+
+    @mock.patch("python_project_wizard.build_project.build_files.build_file")
+    def test_build_static(self, mocked_build: mock.Mock):
+        project = Project(name="merlin project")
+        directories = Directories(os.getcwd(), project)
+        build_static_files(project, directories)
+        mocked_build.assert_any_call(os.path.join(directories.main, "README.md"), "# Merlin Project")
+
+    @mock.patch("python_project_wizard.build_project.build_files.build_file")
+    def test_build_launch_json(self, mockec_build: mock.Mock):
+        project = Project(name="merlin project")
+        directories = Directories(os.getcwd(), project)
+        build_launch_json(project, directories)
+        mockec_build.assert_any_call(os.path.join(directories.dot_vscode, "launch.json"), get_launch_json_content(project))
