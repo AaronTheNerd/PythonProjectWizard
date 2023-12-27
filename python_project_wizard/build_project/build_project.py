3f79f4a336aa1dcbe497a1bb1ba9e5da5ff03634
AaronTheNerd <bargeaaron@gmail.com>
2023-12-27 16:36:25 -0500

    Bug: Fixed small bug wrong function is called in build_project()

--- python_project_wizard/build_project/build_project.py
+++ python_project_wizard/build_project/build_project.py
@@ -1,6 +1,6 @@
 import os
 
-from python_project_wizard.build_project.build_files import build_files
+from python_project_wizard.build_project.build_files import get_and_build_files
 from python_project_wizard.build_project.directories import Directories
 from python_project_wizard.build_project.pipenv import create_pipenv
 from python_project_wizard.project import Project
@@ -22,7 +22,7 @@ from python_project_wizard.project import Project
 def build_project(project: Project):
     directories = build_directories(project)
     create_pipenv(project, directories)
-    build_files(project, directories)
+    get_and_build_files(project, directories)
 
 
 def build_directories(project: Project) -> Directories:
