c2eab745b3880654d850fbac4c81db9d775f1001
AaronTheNerd <bargeaaron@gmail.com>
2025-09-16 15:50:18 -0400

    Chore: Refactored BoolQuestion

--- python_project_wizard/question/bool_question.py
+++ python_project_wizard/question/bool_question.py
@@ -6,10 +6,8 @@ from python_project_wizard.question.question import Question
 
 class BoolQuestion(Question):
     def validate_input_or_default(self, raw_input: str) -> Answer:
-        true_input = re.match("^y", raw_input, re.IGNORECASE)
-        if true_input:
+        if re.match("^y", raw_input, re.IGNORECASE):
             return Answer(True)
-        false_input = re.match("^n", raw_input, re.IGNORECASE)
-        if false_input:
+        if re.match("^n", raw_input, re.IGNORECASE):
             return Answer(False)
         raise ValueError(f"Unexpected value received: `{raw_input}`.")
